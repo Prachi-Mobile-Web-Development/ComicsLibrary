fd413ff66481ec08e852c61bda88cce91cab94f9
Prachi <prachikhisti@gmail.com>
2026-03-31 18:51:51 +0530

    screen navigation structure added

--- app/src/main/java/com/example/comicslibrary/MainActivity.kt
+++ app/src/main/java/com/example/comicslibrary/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.comicslibrary
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.comicslibrary.ui.theme.ComicsLibraryTheme
+import com.example.comicslibrary.view.CharacterDetailsScreen
+import com.example.comicslibrary.view.CollectionScreen
+import com.example.comicslibrary.view.LibraryScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 sealed class Destination(val route: String){
@@ -27,29 +36,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComicsLibraryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(),color= MaterialTheme.colorScheme.background) {
+                    val navController= rememberNavController()
+
+                    CharacterScaffold(navController = navController)
                 }
+
+
             }
         }
     }
 }
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CharacterScaffold(navController: NavHostController){
+     Scaffold(bottomBar = {}) {
+         NavHost(
+             navController = navController,
+             startDestination = Destination.Library.route
+         ) {
+             composable(Destination.Collection.route) {
+                 LibraryScreen()
+
+             }
+             composable(Destination.Library.route) {
+                 CollectionScreen()
+
+
+             }
+             composable(Destination.CharacterDetail.route ) {
+                 CharacterDetailsScreen()
+             }
+
+         }
+     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComicsLibraryTheme {
-        Greeting("Android")
-    }
-}
